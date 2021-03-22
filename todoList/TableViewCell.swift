8b66375d634e51c9cadcb733182aa63258526529
tonyji146@gmail.com <tonyji146@gmail.com>
2021-03-22 21:09:02 +0530

    first commit

---
+++ todoList/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  todoList
+//
+//  Created by Tonywilson Jesuraj on 22/03/21.
+//  Copyright © 2021 Tonywilson Jesuraj. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ todoList/ViewControllerVM.swift
@@ -0,0 +1,9 @@
+//
+//  ViewControllerVM.swift
+//  todoList
+//
+//  Created by Tonywilson Jesuraj on 22/03/21.
+//  Copyright © 2021 Tonywilson Jesuraj. All rights reserved.
+//
+
+import Foundation
